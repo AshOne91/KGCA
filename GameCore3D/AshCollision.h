f70ab394504ba8b42fa5f3714c78323dc3086952
AshOne91 <ldgo91@gmail.com>
2022-10-28 15:19:15 +0900

    no message

--- GameCore3D/AshCollision.h
+++ GameCore3D/AshCollision.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "AshMath.h"
+
+enum class PointPosition
+{
+	CP_FRONT,
+	CP_BACK,
+	CP_ONPLANE,
+	CP_SPANNING
+};
+
+class Collision
+{
+public:
+	TVector3 _vBackPosition;
+	TVector3 _vPosition;
+
+};
